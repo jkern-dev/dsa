aea8437e87b15437191ba30580375e509388d17b
Josh Kern <jkern.dev@gmail.com>
2024-06-03 23:47:52 -0700

    triangle judgement

---
+++ sql/triangle_judgement.sql
@@ -0,0 +1,12 @@
+-- https://leetcode.com/problems/triangle-judgement/description/
+select 
+    x,
+    y,
+    z,
+    case
+        when x+y <= z then "No"
+        when x+z <= y then "No"
+        when y+z <= x then "No"
+        else "Yes"
+    end as triangle
+from Triangle
